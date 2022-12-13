d2a93291b648267fece6fc15d3e7cbc8daa4a70f
Abdirizak Mohamed <abdirizakm123@gmail.com>
2022-12-14 00:15:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+function reverseString(word) {
+  // TODO: implement string reversing functionality
+  return word;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the input string
+  // if the reversed string is the same as the input
+  //   return true
+  // else
+  //   return false
+
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  
+  reverse the input string
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
+
+
 */
 
 /*
   Add written explanation of your solution here
+   A function that takes an input and return true if the string is palindrome nature else return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +49,27 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
